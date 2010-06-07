59961eb8c0731afa45afbf091ac6b3c7be3bb103
David North <davidnorth@railsdog.com>
2010-06-07 13:44:01 -0500

    2 for 1 argument exception raised in some situations due to overriden method_missing without *args parameter

--- related_products_extension.rb
+++ related_products_extension.rb
@@ -22,11 +22,11 @@ class RelatedProductsExtension < Spree::Extension
         RelationType.find_all_by_applies_to(self.to_s, :order => :name)
       end
 
-      def method_missing(method)
+      def method_missing(method, *args)
         relation_type =  self.class.relation_types.detect { |rt| rt.name.downcase.gsub(" ", "_").pluralize == method.to_s.downcase }
 
         if relation_type.nil?
-          super(method)
+          super(method, *args)
         else
           relations.find_all_by_relation_type_id(relation_type.id).map(&:related_to).select {|product| product.deleted_at.nil? && product.available_on <= Time.now()}
         end
