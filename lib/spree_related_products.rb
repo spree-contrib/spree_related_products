d0ebeb5a14e96b3f582517de61e04b78b47fe372
citrus <spencer@citrusme.com>
2011-02-04 10:48:07 -0800

    removed method_missing for 192

--- lib/spree_related_products.rb
+++ lib/spree_related_products.rb
@@ -15,16 +15,16 @@ module SpreeRelatedProducts
           RelationType.find_all_by_applies_to(self.to_s, :order => :name)
         end
 
-        def method_missing(method, *args)
-          relation_type =  self.class.relation_types.detect { |rt| rt.name.downcase.gsub(" ", "_").pluralize == method.to_s.downcase }
-
-          if relation_type.nil?
-            super
-          else
-            relations.find_all_by_relation_type_id(relation_type.id).map(&:related_to).select {|product| product.deleted_at.nil? && product.available_on <= Time.now()}
-          end
-
-        end
+        #def method_missing(method, *args)
+        #  relation_type =  self.class.relation_types.detect { |rt| rt.name.downcase.gsub(" ", "_").pluralize == method.to_s.downcase }
+        #
+        #  if relation_type.nil?
+        #    super
+        #  else
+        #    relations.find_all_by_relation_type_id(relation_type.id).map(&:related_to).select {|product| product.deleted_at.nil? && product.available_on <= Time.now()}
+        #  end
+        #
+        #end
       end
 
       Admin::ProductsController.class_eval do
