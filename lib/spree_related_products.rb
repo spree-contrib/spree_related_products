9846d144fe169a8f0703cf78ec1749bb55af80e5
Spencer Steffen <spencer@citrusme.com>
2011-06-30 10:50:50 -0700

    updated products controller for 0.60.x

--- lib/spree_related_products.rb
+++ lib/spree_related_products.rb
@@ -32,7 +32,7 @@ module SpreeRelatedProducts
 
       Admin::ProductsController.class_eval do
         def related
-          load_object
+          load_resource
           @relation_types = Product.relation_types
         end
       end
