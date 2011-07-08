11b4c2d655aace648190078d705299ba1751aa0f
Adam Jahn <ajjahn@gmail.com>
2011-07-08 03:31:32 -0400

    "Reapply "Updates for edge support"
    This reverts commit 3f593bd0e950400d3d28f297e2506b3af40f0004.
    
    Conflicts:
    
    	app/views/admin/products/_related_products.html.erb
    	lib/spree_related_products.rb

--- lib/spree_related_products.rb
+++ lib/spree_related_products.rb
@@ -1,42 +1,15 @@
 require 'spree_core'
-require 'spree_related_products_hooks'
 
 module SpreeRelatedProducts
   class Engine < Rails::Engine
 
     def self.activate
 
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+
       # Calculator::RelatedProductDiscount.register
-
-      Product.class_eval do
-        has_many :relations, :as => :relatable
-
-        def self.relation_types
-          RelationType.find_all_by_applies_to(self.to_s, :order => :name)
-        end
-
-        def method_missing(method, *args)
-          relation_type =  self.class.relation_types.detect { |rt| rt.name.downcase.gsub(" ", "_").pluralize == method.to_s.downcase }
-            
-          # Fix for Ruby 1.9
-          raise NoMethodError if method == :to_ary
-        
-          if relation_type.nil?
-            super
-          else
-            relations.find_all_by_relation_type_id(relation_type.id).map(&:related_to).select {|product| product.deleted_at.nil? && product.available_on <= Time.now()}
-          end
-
-        end
-      end
-
-      Admin::ProductsController.class_eval do
-        def related
-          load_resource
-          @relation_types = Product.relation_types
-        end
-      end
-
     end
 
     config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/models/calculator)
--- lib/spree_related_products_hooks.rb
+++
@@ -1,9 +0,0 @@
-class SpreeRelatedProductsHooks < Spree::ThemeSupport::HookListener
-
-  insert_after :admin_product_tabs, "admin/products/related_products"
-
-  insert_after :admin_configurations_menu do
-    "<%= configurations_menu_item(I18n.t('relation_types'), admin_relation_types_url, I18n.t('manage_relation_types')) %>"
-  end
-
-end
