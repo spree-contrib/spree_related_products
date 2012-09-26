00286404b41f950821119e70099551b0e62cab88
Jeff Dutil <jdutil21@gmail.com>
2012-09-25 23:19:08 -0400

    Remove unused & incorrect copy/pasted code.

--- lib/generators/spree_related_products/install/install_generator.rb
+++ lib/generators/spree_related_products/install/install_generator.rb
@@ -2,26 +2,19 @@ module SpreeRelatedProducts
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      # def add_javascripts
-      #   append_file "app/assets/javascripts/store/all.js", "//= require store/spree_wishlist\n" 
-      # end
-
-      # def add_stylesheets
-      #   inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_wishlist\n", :before => /\*\//, :verbose => true
-      # end
-
       def add_migrations
         run 'rake railties:install:migrations FROM=spree_related_products'
       end
 
       def run_migrations
-         res = ask "Would you like to run the migrations now? [Y/n]"
-         if res == "" || res.downcase == "y"
-           run 'rake db:migrate'
-         else
-           puts "Skiping rake db:migrate, don't forget to run it!"
-         end
+        res = ask "Would you like to run the migrations now? [Y/n]"
+        if res == "" || res.downcase == "y"
+          run 'rake db:migrate'
+        else
+          puts "Skipping rake db:migrate, don't forget to run it!"
+        end
       end
+
     end
   end
 end
