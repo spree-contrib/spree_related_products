b1382a3c2a3da05811beb90993c9b76de2b5364e
geermc4 <german@3drobotics.com>
2012-11-02 13:17:10 -0700

    Update to make it work on 1.3

--- app/overrides/admin_decorator.rb
+++ app/overrides/admin_decorator.rb
@@ -4,8 +4,8 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :partial => "spree/admin/products/related_products",
                      :disabled => false)
 
-Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                      :name => "add_configuration_line",
-                     :insert_bottom => "[data-hook='admin_configurations_menu']",
-                     :text => "<%= configurations_menu_item(I18n.t('relation_types'), admin_relation_types_url, I18n.t('manage_relation_types')) %>",
+                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
+                     :text => "<%= configurations_sidebar_menu_item(I18n.t('manage_relation_types'), admin_relation_types_url) %>",
                      :disabled => false)
