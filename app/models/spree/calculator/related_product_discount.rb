2279257fca73edcaede21c08fef490dbefcc0dab
Samuel <samuel@innovega.net>
2013-03-13 01:08:30 +0100

    fix error eligible?

--- app/models/spree/calculator/related_product_discount.rb
+++ app/models/spree/calculator/related_product_discount.rb
@@ -1,7 +1,5 @@
 module Spree
   class Calculator::RelatedProductDiscount < Spree::Calculator
-    # preference :item_total_threshold, :decimal, :default => 5
-    # attr_accessible :preferred_item_total_threshold
 
     def self.description
       I18n.t("related_product_discount")
@@ -18,13 +16,13 @@ module Spree
       return unless eligible?(order)
       total = order.line_items.inject(0) do |total, line_item|
         relations =  Spree::Relation.find(:all, :conditions => ["discount_amount <> 0.0 AND relatable_type = ? AND relatable_id = ?", "Spree::Product", line_item.variant.product.id])
-        discount_applies_to = relations.map {|rel| rel.related_to.master }
+        discount_applies_to = relations.map {|rel| rel.related_to }
 
         order.line_items.each do |li|
-          if discount_applies_to.include? li.variant
-            discount = relations.detect {|rel| rel.related_to.master == li.variant}.discount_amount
+          if discount_applies_to.include? li.product
+            discount = relations.detect {|rel| rel.related_to == li.product}.discount_amount
 
-            discount = li.variant.price * discount / 100
+            discount = li.price * discount / 100
 
             total += if li.quantity < line_item.quantity
               (discount * li.quantity)
@@ -41,7 +39,8 @@ module Spree
     end
 
     def eligible?(order)
-      order.line_items.any? { |line_item| Spree::Relation.exists?(["discount_amount <> 0.0 AND relatable_type = ? AND relatable_id = ?", "Spree::Product", line_item.variant.product.id])}
+      product_ids = order.line_items.map{ |line_item| line_item.variant.product_id}.uniq
+      Spree::Relation.where(["discount_amount <> 0.0 AND related_to_type = ? AND related_to_id IN (?)", "Spree::Product", product_ids]).exists?
     end
 
   end
