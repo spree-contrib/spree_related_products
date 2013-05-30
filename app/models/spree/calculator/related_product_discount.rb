75a9cd0deffaef52bd3f7079154a4874e5d0ed69
Tobias Bohwalli <hi@futhr.io>
2013-05-31 00:09:38 +0200

    Fix Spree locale namespace.

--- app/models/spree/calculator/related_product_discount.rb
+++ app/models/spree/calculator/related_product_discount.rb
@@ -1,9 +1,9 @@
 module Spree
   class Calculator::RelatedProductDiscount < Spree::Calculator
-    preference :item_total_threshold, :decimal, :default => 5
+    preference :item_total_threshold, :decimal, default: 5
 
     def self.description
-      Spree.t("related_product_discount")
+      Spree.t(:related_product_discount)
     end
 
     def compute(object)
@@ -16,7 +16,7 @@ module Spree
 
       return unless eligible?(order)
       total = order.line_items.inject(0) do |total, line_item|
-        relations =  Spree::Relation.find(:all, :conditions => ["discount_amount <> 0.0 AND relatable_type = ? AND relatable_id = ?", "Spree::Product", line_item.variant.product.id])
+        relations =  Spree::Relation.find(:all, conditions: ["discount_amount <> 0.0 AND relatable_type = ? AND relatable_id = ?", "Spree::Product", line_item.variant.product.id])
         discount_applies_to = relations.map {|rel| rel.related_to.master }
 
         order.line_items.each do |li|
