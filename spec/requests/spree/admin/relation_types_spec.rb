e2faeb867546014e9563aaa0b3c5aacca642406c
root <root@frank-virtual-machine.(none)>
2013-07-07 18:42:08 +0800

    Add Request Spec/Fix bugs in views/spree/admin/relation_types/new.html

---
+++ spec/requests/spree/admin/relation_types_spec.rb
@@ -0,0 +1,78 @@
+require 'spec_helper'
+
+describe "Manage Relation Types", :type => "controller" do
+  stub_authorization!
+
+  before(:each) do
+
+    @relation_type = Spree::RelationType.create(name: "Benz", applies_to: "Spree::Product")
+    @relation_type.save()
+
+    @relation_type = Spree::RelationType.create(name: "BMW", applies_to: "Spree::Product")
+    @relation_type.save()
+
+    visit spree.admin_path
+    click_link "Configuration"
+    click_link "Manage Relation Types"
+
+    # visit spree.admin_relation_types_path
+
+  end
+
+  context "create" do
+    it "should be able to create a new relation type" do
+
+      # create(:relation_type, :name => 'test', :applies_to => "Spree::Product")
+      
+      click_link "New Relation Type"
+
+      fill_in "Name", :with => "bullock cart"
+      fill_in "Applies To", :with => "Spree:Products"
+
+      click_button "Create"
+
+      page.should have_content("successfully created!")
+      
+      expect(current_path).to eql(spree.admin_relation_types_path)
+    end
+
+    it "should show validation errors", :js => true do
+      click_link "New Relation Type"
+
+      fill_in "Name", :with => ""
+      click_button "Create"
+      
+      page.should have_content("Name can't be blank")
+    end
+
+  end
+
+  context "show" do
+    it "should display existing relation types" do
+      within_row(1) do
+        column_text(1).should == "Benz"
+        column_text(2).should == "Spree::Product"
+        column_text(3).should == ""
+      end
+    end
+  end
+  
+  context "edit" do
+    before(:each) do
+      within_row(1) { click_icon :edit }
+    end
+
+    it "should allow an admin to edit an existing relation type" do
+      fill_in "Name", :with => "model 99"
+      click_button "Update"
+      page.should have_content("successfully updated!")
+      page.should have_content("model 99")
+    end
+
+    # it "should show validation errors if there are any" do
+    #   click_button "Update"
+    #   page.should have_content("Name can't be blank")
+    # end
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,6 +30,7 @@ require 'spree/testing_support/factories'
 require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/url_helpers'
+require 'spree/testing_support/capybara_ext'
 
 RSpec.configure do |config|
   config.include Capybara::DSL, type: :request
