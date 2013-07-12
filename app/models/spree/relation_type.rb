bde45856ce88d4c789426ba2cd869b008be39c99
Tobias Bohwalli <hi@futhr.io>
2013-07-12 13:49:37 +0200

    Grouped validations.

--- app/models/spree/relation_type.rb
+++ app/models/spree/relation_type.rb
@@ -1,9 +1,8 @@
 class Spree::RelationType < ActiveRecord::Base
   has_many :relations, dependent: :destroy
+
   attr_accessible :name, :applies_to, :description
 
-  validates :name, :presence => true
-  validates :applies_to, :presence => true
-
+  validates :name, :applies_to, presence: true
   validates :name, uniqueness: true
 end
