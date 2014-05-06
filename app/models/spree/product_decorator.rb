4c5124b47b0b9005418bd9bf5886bf5cbc6820f7
Washington Luiz <huoxito@gmail.com>
2014-05-05 21:25:55 -0300

    Take better advantage of active record relations
    Product#relations_for_relation_type could return an array with `nil`
    elements due to the collect / detect. I guess no need to use ruby
    compact since filtering using active record relation already takes care
    of that
    
    Possible fix for #75
    
    Conflicts:
    	app/models/spree/product_decorator.rb

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -86,10 +86,12 @@ Spree::Product.class_eval do
     result = self.class.where(:id => related_ids)
 
     # Merge in the relation_filter if it's available
-    result = result.merge(self.class.relation_filter.references(self).to_a) if relation_filter
+    result = result.merge(self.class.relation_filter) if relation_filter
 
     # make sure results are in same order as related_ids array  (position order)
-    result = related_ids.collect {|id| result.detect {|x| x.id == id} } if result.present?
+    if result.present?
+      result.where(id: related_ids).order(:position)
+    end
 
     result
   end
@@ -100,5 +102,4 @@ Spree::Product.class_eval do
   def relation_filter
     self.class.relation_filter
   end
-
 end
