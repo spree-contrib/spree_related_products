6d5fe1907e16891d6110638bbf01ce109c41a237
Gustavo Robles <gustavo.robles@crowdint.com>
2014-06-25 13:11:58 -0500

    Added API for Relations
    Fixes #93
    
    Conflicts:
    	spec/factories/relation_factory.rb
    	spec/factories/relation_type_factory.rb
    	spec/spec_helper.rb

---
+++ app/controllers/spree/api/relations_controller.rb
@@ -0,0 +1,64 @@
+module Spree
+  module Api
+    class RelationsController < Spree::Api::BaseController
+      before_filter :load_data, :only => [:create, :destroy]
+      before_filter :find_relation, only: [:update, :destroy]
+
+      def create
+        authorize! :create, Relation
+        @relation = @product.relations.new(relation_params)
+        @relation.relatable = @product
+        @relation.related_to = Spree::Variant.find(relation_params[:related_to_id]).product
+        if @relation.save
+          respond_with(@relation, status: 201, default_template: :show)
+        else
+          invalid_resource!(@relation)
+        end
+      end
+
+      def update
+        authorize! :update, Relation
+        if @relation.update_attributes(relation_params)
+          respond_with(@relation, status: 200, default_template: :show)
+        else
+          invalid_resource!(@relation)
+        end
+      end
+
+      def update_positions
+        authorize! :update, Relation
+        params[:positions].each do |id, index|
+          model_class.where(:id => id).update_all(:position => index)
+        end
+
+        respond_to do |format|
+          format.js  { render :text => 'Ok' }
+        end
+      end
+
+      def destroy
+        authorize! :destroy, Relation
+        @relation.destroy
+        respond_with(@relation, status: 204)
+      end
+
+      private
+
+      def relation_params
+        params.require(:relation).permit(:related_to, :relation_type, :relatable, :related_to_id, :discount_amount, :relation_type_id, :related_to_type, :position)
+      end
+
+      def load_data
+        @product = Spree::Product.friendly.find(params[:product_id])
+      end
+
+      def find_relation
+        @relation = Relation.find(params[:id])
+      end
+
+      def model_class
+        Spree::Relation
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,13 @@ require 'database_cleaner'
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
 require 'spree/testing_support/factories'
+
+# include local factories
+Dir["#{File.dirname(__FILE__)}/factories/**/*.rb"].each do |f|
+  fp =  File.expand_path(f)
+  require fp
+end
+
 require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/url_helpers'
@@ -25,7 +32,7 @@ FactoryGirl.find_definitions
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  config.include Spree::TestingSupport::ControllerRequests
+  config.include Spree::TestingSupport::ControllerRequests, type: :controller
   config.include Spree::TestingSupport::UrlHelpers
 
   config.mock_with :rspec
