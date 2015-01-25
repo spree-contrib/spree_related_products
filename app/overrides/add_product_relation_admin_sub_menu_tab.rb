0584ea1fba9f979ffb18ae38c7def3c937264821
Tobias Bohwalli <hi@futhr.io>
2015-01-25 11:04:01 +0100

    Remove Deface option "disabled".
    Disabled option is unnecessary and may just raise deprecation warnings.

--- app/overrides/add_product_relation_admin_sub_menu_tab.rb
+++ app/overrides/add_product_relation_admin_sub_menu_tab.rb
@@ -2,6 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/sub_menu/_product',
   name: 'add_product_relation_admin_sub_menu_tab',
   insert_bottom: '[data-hook="admin_product_sub_tabs"]',
-  text: '<%= tab :relation_types, label: Spree::RelationType.model_name.human(count: :many) %>',
-  disabled: false
+  text: '<%= tab :relation_types, label: Spree::RelationType.model_name.human(count: :many) %>'
 )
--- app/overrides/add_related_product_admin_tabs.rb
+++ app/overrides/add_related_product_admin_tabs.rb
@@ -2,6 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/_product_tabs',
   name: 'add_related_products_admin_tab',
   insert_bottom: '[data-hook="admin_product_tabs"]',
-  partial: 'spree/admin/products/related_products',
-  disabled: false
+  partial: 'spree/admin/products/related_products'
 )
