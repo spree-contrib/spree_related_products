6c411c74d9fc8d19256af6583cc1a7e3b3734cfa
Anthony D'Addeo <anthony@personalwine.com>
2015-05-13 14:40:48 -0500

    touch relatable when updated

--- app/models/spree/relation.rb
+++ app/models/spree/relation.rb
@@ -1,6 +1,6 @@
 class Spree::Relation < ActiveRecord::Base
   belongs_to :relation_type
-  belongs_to :relatable, polymorphic: true
+  belongs_to :relatable, polymorphic: true, touch: true
   belongs_to :related_to, polymorphic: true
 
   validates :relation_type, :relatable, :related_to, presence: true
