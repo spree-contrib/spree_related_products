07aed7ed6d2a15ebdf939e7ff2bbbd6bdd49b229
Pablo Torre <pablotorre81@gmail.com>
2016-06-07 13:08:24 +0200

    Fix bug: undefined method `empty?' for nil:NilClass #127

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,10 +1,7 @@
-module Spree
-  module Admin
-    ProductsController.class_eval do
-      def related
-        load_resource
-        @relation_types = Spree::Product.relation_types
-      end
-    end
+Spree::Admin::ProductsController.class_eval do
+  def related
+    load_resource
+    @relation_types = Spree::Product.relation_types
   end
 end
+
