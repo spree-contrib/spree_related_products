f9f50f6847a8f408a0d95d1c2c80ce7e3e7ac658
Krzysztof Rybka <krzysztof.rybka@gmail.com>
2016-08-06 14:00:14 +0200

    randomize rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,6 +31,8 @@ RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.syntax = :expect
   end
+
+  config.order = :random
 end
 
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |file| require file }
