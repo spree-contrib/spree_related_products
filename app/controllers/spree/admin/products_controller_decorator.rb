4b131834b84c4ab5e7e522bf62d75edccc2a9b44
Punkbooster <ugliar.arsen@gmail.com>
2019-09-24 12:29:03 +0200

    Rewrite product decorator to use prepend insted of class eval

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,6 +1,12 @@
-Spree::Admin::ProductsController.class_eval do
-  def related
-    load_resource
-    @relation_types = Spree::Product.relation_types
+module Spree
+  module Admin
+    module ProductsControllerDecorator
+      def related
+        load_resource
+        @relation_types = Spree::Product.relation_types
+      end
+    end
   end
 end
+
+::Spree::Admin::ProductsController.prepend ::Spree::Admin::ProductsControllerDecorator
