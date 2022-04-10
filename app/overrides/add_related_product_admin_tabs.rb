51ce2a6d787ea7b316215731948a13533e18be52
Yury Batenko <jurbat@gmail.com>
2022-04-10 17:14:20 +0700

    Fix admin menue

--- app/overrides/add_related_product_admin_tabs.rb
+++ app/overrides/add_related_product_admin_tabs.rb
@@ -2,5 +2,6 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/_product_tabs',
   name: 'add_related_products_admin_tab',
   insert_bottom: '[data-hook="admin_product_tabs"]',
-  partial: 'spree/admin/products/related_products'
+  partial: 'spree/admin/products/related_products',
+  original: 'f73e3520bb51c6b845c3f36f013c0d824f64bca4'
 )
