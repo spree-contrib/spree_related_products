29e1e6dbcc264a2c62f89924e9de0811dcc3025a
mrbrdo <mrbrdo@gmail.com>
2022-12-20 20:39:20 +0100

    fix admin product tab for Spree 4.5+

--- app/overrides/add_related_product_admin_tabs.rb
+++ app/overrides/add_related_product_admin_tabs.rb
@@ -1,7 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_product_tabs',
   name: 'add_related_products_admin_tab',
-  insert_bottom: '[data-hook="admin_product_tabs"]',
-  partial: 'spree/admin/products/related_products',
-  original: 'c4791d35732b95bae11a32d2f9b6d0e6155f1fea'
+  insert_after: ':last-child',
+  partial: 'spree/admin/products/related_products'
 )
