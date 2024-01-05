8e92191b1cae642ae143e6236ab435d698791fe4
mrbrdo <mrbrdo@gmail.com>
2024-01-05 20:38:15 +0100

    Temp fix for adding Related Products tab to product admin page

--- app/overrides/add_related_product_admin_tabs.rb
+++ app/overrides/add_related_product_admin_tabs.rb
@@ -1,6 +1,14 @@
+insert_after_selector =
+  if defined?(Deface::LambdaMatcher)
+    Deface::LambdaMatcher.new do |document|
+      [document.xpath(".//erb").last]
+    end
+  else
+    ':last-child' # TODO: this doesn't work correctly
+  end
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_product_tabs',
   name: 'add_related_products_admin_tab',
-  insert_after: ':last-child',
+  insert_after: insert_after_selector,
   partial: 'spree/admin/products/related_products'
 )
